f8c8555faf1c759fbc7c7ad7ec4a9abb2d9e5801
florianMarco <florian.marco@live.com>
2019-11-18 13:52:29 +0100

    update materiale

--- Assignment1c/gen-src/it/polito/dp2/BIB/sol1/jaxb/package-info.java
+++ Assignment1c/gen-src/it/polito/dp2/BIB/sol1/jaxb/package-info.java
@@ -2,7 +2,7 @@
 // Questo file è stato generato dall'architettura JavaTM per XML Binding (JAXB) Reference Implementation, v2.2.8-b130911.1802 
 // Vedere <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Qualsiasi modifica a questo file andrà persa durante la ricompilazione dello schema di origine. 
-// Generato il: 2019.11.17 alle 06:57:13 PM CET 
+// Generato il: 2019.11.18 alle 12:55:33 PM CET 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://pad.polito.it/dp2/biblio_e")
---
+++ Assignment1c/src/it/polito/dp2/BIB/sol1/ItemReaderImpl.java
@@ -0,0 +1,33 @@
+package it.polito.dp2.BIB.sol1;
+
+import it.polito.dp2.BIB.ItemReader;
+
+import java.util.Set;
+
+public class ItemReaderImpl implements ItemReader {
+    private ItemReader itemReader;
+
+    public ItemReaderImpl(ItemReader itemReader) {
+        this.itemReader = itemReader;
+    }
+
+    @Override
+    public String[] getAuthors() {
+        return itemReader.getAuthors();
+    }
+
+    @Override
+    public String getTitle() {
+        return itemReader.getTitle();
+    }
+
+    @Override
+    public String getSubtitle() {
+        return itemReader.getSubtitle();
+    }
+
+    @Override
+    public Set<ItemReader> getCitingItems() {
+        return itemReader.getCitingItems();
+    }
+}
